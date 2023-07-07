99afd573fc44ab78553e75421ff7af11f7ef205b
12108541muhamad <muhamaddwiputranovriansyah@smkwikrama.sch.id>
2023-07-07 13:44:40 +0700

    Fix about section

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -2,11 +2,11 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 
 const NavbarComp = () => {
   return (
-    <div className='navigation sticky-top'>
+    <div className='navigation sticky-top mb-5'>
       <Navbar expand="lg" id='navbar'>
         <Container>
           <Navbar.Brand className='space-mono text-white' href="#hero">Wiii.</Navbar.Brand>
-          <Navbar.Toggle lg={false} sm={false} aria-controls="basic-navbar-nav" data-bs-theme='dark' className='custom-nav-tog border-0' />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" data-bs-theme='dark' className='custom-nav-tog border-0' />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link className='space-mono text-white' href="#about">About</Nav.Link>
